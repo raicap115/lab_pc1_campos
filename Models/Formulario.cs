1cdaf8e0eb1b8dcca3d2c3080a305c888e18047f
Raí Campos <raicampas23@gmail.com>
2020-10-19 12:53:37 -0500

    arreglos

--- Models/Formulario.cs
+++ Models/Formulario.cs
@@ -1,39 +1,49 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace lab_pc1_campos.Models
 {
+    [Table("t_solicitu_bono")]
     public class Formulario 
     {
-        
-        
+
         [Required(ErrorMessage="Ingrese su nombre")]  
-        [Display(Name="Nombre")]  
+        [Display(Name="nombre")] 
+        [Column("nombre")] 
         public string Nombre { get; set; }
         
         [Required(ErrorMessage="Ingrese sus apellidos")]  
-        [Display(Name="Apellido")]
+        [Display(Name="apellido")]
+        [Column("apellido")]
+
         public string Apellido { get; set; }
         
         [Required(ErrorMessage="Ingrese su distrito de residencia")]  
         [Display(Name="Distrito")]
+        [Column("distrito")]
         public String Distrito { get; set; }
 
         [Required(ErrorMessage="Ingrese su banco afiliado")]  
         [Display(Name="Bancos")]
+        [Column("bancos")]
         public string Bancos { get; set; }
 
         [Required(ErrorMessage="Ingrese su edad")]  
         [Display(Name="Edad")]
+        [Column("edad")]
         public string Edad { get; set; }
         
         [Display(Name="Sexo")]
+        [Column("sexo")]
         public string Sexo { get; set; }
 
         [Display(Name="Autor")]
+        [Column("autor")]
         public string Autor { get; }
 
-        [Display(Name="Output")] 
+        [Display(Name="Output")]
+        [NotMapped]
          public string Respuesta { get; set; }
 
     }
